109efd5063a43dc290b014351d46fabf2943da64
Marcos Porto <mosther_marcos007@hotmail.com>
2023-11-25 18:04:13 -0300

    alters project

--- src/components/DevCreate.tsx
+++ src/components/DevCreate.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export function DevCreate() {
   return (
-    <footer className="text-center dark:text-white text-black absolute bottom-0">
+    <footer className="text-center dark:text-white text-black">
       <p className="text-sm">Desenvolvido por Marcos Porto</p>
       <p className="text-xs">&copy; Marcos Porto</p>
     </footer>
